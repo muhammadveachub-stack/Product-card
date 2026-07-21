962bfbe9e3f9f26a5473ce14191124a9b5c644e5
muhammadveachub-stack <muhammad.veachub@gmail.com>
2026-07-21 03:09:17 +0300

    Added js file and buttons

---
+++ main.js
@@ -0,0 +1,65 @@
+// Покраска всех карточек
+
+const productCards = document.querySelectorAll('.products__item');
+const changeColorAllCardButton= document.querySelector('#change-color-all-card');
+const greenColor = '#00ff00';
+const blueColor = '#1100ff'
+
+changeColorAllCardButton.addEventListener('click', () => {
+  productCards.forEach((card) => card.style.backgroundColor = greenColor)
+})  
+
+// Покраска первой карточки
+
+const firstProductCard = document.querySelector('.products__item');
+const changeColorFirstCardButton= document.querySelector('#change-color-first-card');
+ 
+changeColorFirstCardButton.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = blueColor
+}) 
+
+// Открыть google
+
+const openGoogleButton = document.querySelector('#open-google');
+
+openGoogleButton.addEventListener( 'click', openGoogle)
+
+function openGoogle() {
+  const answer = confirm('Вы внатуре хотите открыть Google?');
+
+  if (answer === true) {
+    window.open('https://google.com')
+  }
+  else {
+    return;
+  }
+}
+
+// Вывод консоль лог
+
+const outputLogButton = document.querySelector('#output-console-log');
+
+outputLogButton.addEventListener('click', () => outputConsoleLog('ДЗ №4'))
+
+function outputConsoleLog(message) {
+  console.log(message);
+  alert(10)
+}
+  
+
+//Вывод лога заголовка
+
+const outputTitle = document.querySelector('#title-console');
+
+outputTitle.addEventListener('mouseover', () => {
+  console.log(outputTitle.textContent)
+})
+
+
+//Кнопка, меняющая цвет
+
+const buttonToggle = document.querySelector('#toggle-color-button');
+
+buttonToggle.addEventListener('click', () => {
+buttonToggle.classList.toggle('button-blue')
+})
